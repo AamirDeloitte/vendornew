ad6939b31fb44cf4e37f21ea9da54cff69fd6cd5
Pandurekha Chaitanya <chaitu2591@gmail.com>
2019-02-15 14:26:11 +0000

    c

--- webapp/Component.js
+++ webapp/Component.js
@@ -19,25 +19,25 @@ sap.ui.define([
 		init: function () {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-<<<<<<< HEAD
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
-			// enable routing
-			this.getRouter().initialize();
-=======
-
-			// enable routing
-			this.getRouter().initialize();
-
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
 			// enable routing
 			this.getRouter().initialize();
 
->>>>>>> refs/heads/master
+
+			// enable routing
+			this.getRouter().initialize();
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+			// enable routing
+			this.getRouter().initialize();
+
+
 		}
 	});
 });
